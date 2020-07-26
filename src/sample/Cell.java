c309d6e6985cc60dbf3d390eace3af8d3028820e
Maximilian Mitterrutzner <maximitterrutzner@gmail.com>
2020-07-27 00:59:38 +0200

    Remove obsolete IntegerProperty value in Cell class

--- src/sample/Cell.java
+++ src/sample/Cell.java
@@ -1,25 +1,19 @@
 package sample;
 
 import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
-public class Cell extends Pane {
-    private IntegerProperty value;
-
+class Cell extends Pane {
     Cell(IntegerProperty bounded) {
-        value = new SimpleIntegerProperty();
-        value.bind(bounded);
-
         this.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
         this.setPrefWidth(50);
         this.setPrefHeight(50);
         this.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, null)));
 
-        value.addListener(e -> {
+        bounded.addListener(e -> {
             Color color = Color.BLUE;
-            switch (value.get()) {
+            switch (bounded.get()) {
                 case -1:
                     color = Color.RED;
                 break;
